b162376520f05cd78900cad4e791e5bf471c887d
KovacAca <kovacaleksandar1@gmail.com>
2021-03-14 01:02:57 +0100

    footer html

--- index.php
+++ index.php
@@ -21,5 +21,29 @@
             <li><a href=""><i class="fas fa-search"></i></a></li>
         </ul>
     </nav>
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <div class="col">
+            <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
+            </div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Main street 2, Novi sad</p>
+                <p>Srbija</p>
+            </div>
+            <div class="col">
+            <i class="fas fa-envelope"></i>
+                <p>info@marketing.com</p>
+                <p>office@marketing.com</p>
+            </div>
+            <div class="col">
+            <i class="fas fa-phone"></i>
+                <p>021 456 123</p>
+                <p>063 123 456</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
