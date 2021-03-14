2d5339732256f7af764aa284fd6bf369ffa98cb0
KovacAca <kovacaleksandar1@gmail.com>
2021-03-14 01:20:26 +0100

    footer css i partials

--- index.php
+++ index.php
@@ -1,49 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Business</title>
-     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet"> 
-     <link rel="stylesheet" href="scss/style.css">
-</head>
-<body>
-    <!-- nav -->
-    <nav>
-        <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">team</a></li>
-            <li><a href="">Sign up</a></li>
-            <li><a href=""><i class="fas fa-search"></i></a></li>
-        </ul>
-    </nav>
+<?php include "partials/header.php" ?>
 
-    <!-- footer -->
-    <footer>
-        <div class="container">
-            <div class="col">
-            <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
-            </div>
-            <div class="col">
-                <i class="fas fa-map-marker-alt"></i>
-                <p>Main street 2, Novi sad</p>
-                <p>Srbija</p>
-            </div>
-            <div class="col">
-            <i class="fas fa-envelope"></i>
-                <p>info@marketing.com</p>
-                <p>office@marketing.com</p>
-            </div>
-            <div class="col">
-            <i class="fas fa-phone"></i>
-                <p>021 456 123</p>
-                <p>063 123 456</p>
-            </div>
-        </div>
-    </footer>
-</body>
-</html>
+    <h1>Ovde cemo sutra content</h1>
+
+<?php include "partials/footer.php" ?>
+
+    
